9c8ddbc1abac367b25c588ba8e0310bc37b68b2c
guilhermetcltransporte <95231918+guilhermetcltransporte@users.noreply.github.com>
2024-12-02 17:10:04 -0300

    dev

--- functions/server.js
+++ functions/server.js
@@ -4,17 +4,14 @@ import serverless from 'serverless-http'
 const app = express()
 const router = Router()
 
-// Rotas
-router.get('/', (req, res) => {
-  res.json({ message: 'Bem-vindo ao Express com Netlify!' })
-})
-
-router.get('/api/hello', (req, res) => {
+router.get('/hello', (req, res) => {
   const name = req.query.name || 'Mundo'
   res.json({ message: `Olá, ${name}!` })
 })
 
 app.use('/.netlify/functions/server', router)
+
+app.use('/api', router)
 
 // Exportar para Netlify
 const _router = router
