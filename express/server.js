338e8fb3cdf4818a2e534cded48e1fe7637882b3
guilhermetcltransporte <95231918+guilhermetcltransporte@users.noreply.github.com>
2024-12-02 15:10:16 -0300

    dev

--- express/server.js
+++ express/server.js
@@ -1,22 +1,18 @@
-'use strict';
-const express = require('express');
-const path = require('path');
-const serverless = require('serverless-http');
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import routes from './routes.js';
+
 const app = express();
-const bodyParser = require('body-parser');
 
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
-
+// Middleware para parsing do corpo das requisições
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
-module.exports = app;
-module.exports.handler = serverless(app);
+// Rotas da aplicação
+app.use('/.netlify/functions/server', routes);
+
+// Servir arquivo estático
+const __dirname = path.resolve();
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+export default app;
