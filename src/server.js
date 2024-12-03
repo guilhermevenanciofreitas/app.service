5acaa7a98bd39fe47056ccc5d9478155f1752528
guilhermetcltransporte <95231918+guilhermetcltransporte@users.noreply.github.com>
2024-12-03 11:04:22 -0300

    dev

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express, { Router } from 'express'
+import cors from 'cors'
 import serverless from 'serverless-http'
 import { LoginRoute } from './routes/login/login.route.js'
 
@@ -12,10 +13,19 @@ class App {
   }
 
   initializeMiddlewares() {
+
+    const corsOptions = {
+      origin: '*',
+      exposedHeaders: ['Last-Acess', 'Expire-In'],
+    }
+
+    this.express.use(cors(corsOptions))
     this.express.use(express.json())
+
   }
 
   initializeRoutes() {
+
 
     this.express.use('/api/login', new LoginRoute().router)
 
