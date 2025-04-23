c3549112286dc9981946e5b02b4596a6a2272079
guilhermetcltransporte <guilherme.venancio@tcltransporte.com.br>
2025-04-23 17:22:16 -0300

    app

---
+++ app.sales/src/pages/left-page-2.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Page, Navbar, Block } from 'framework7-react';
+
+const LeftPage2 = () => (
+  <Page>
+    <Navbar title="Left Page 2" backLink="Back" />
+    <Block strong>
+      <p>Maxime tempore, repellendus? Sequi saepe provident quas, neque assumenda dicta odit eaque nesciunt facere mollitia totam voluptate aspernatur vel corporis repudiandae commodi ad cumque repellendus. Saepe officia iure repellat repellendus.</p>
+    </Block>
+  </Page>
+);
+
+export default LeftPage2;
